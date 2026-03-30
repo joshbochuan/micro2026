f08d3f9de72324ca46567abf5e9e60e9093899e5
joshbochuan <joshbochuan@gmail.com>
2026-03-30 21:13:35 +0800

    add packages preset

---
+++ packages/packages.vhd
@@ -0,0 +1,29 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.all; -- uses all packages available in this directory
+use work.adder_pkg.all;
+use work.sevenseg_pkg.all;
+
+entity packages is
+	port (
+		SW:   in std_logic_vector(17 downto 0);
+		KEY:  in std_logic_vector(3 downto 0);
+		LEDR: out std_logic_vector(17 downto 0);
+		LEDG: out std_logic_vector(8 downto 0);
+		HEX0: out std_logic_vector(6 downto 0);
+		HEX1: out std_logic_vector(6 downto 0);
+		HEX2: out std_logic_vector(6 downto 0);
+		HEX3: out std_logic_vector(6 downto 0);
+		HEX4: out std_logic_vector(6 downto 0);
+		HEX5: out std_logic_vector(6 downto 0);
+		HEX6: out std_logic_vector(6 downto 0);
+		HEX7: out std_logic_vector(6 downto 0)
+	);
+end packages;
+
+architecture structural of packages is
+   signal tmp: std_logic;
+begin
+	-- do something
+	HEX0 <= "0000000";
+end structural;
---
+++ packages/sevenseg_pkg.vhd
@@ -0,0 +1,64 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+package sevenseg_pkg is
+    subtype nibble_t is STD_LOGIC_VECTOR(3 downto 0);
+    subtype seg7_t   is STD_LOGIC_VECTOR(6 downto 0);
+    function hex_to_7seg(wxyz : nibble_t) return seg7_t;
+end sevenseg_pkg;
+
+package body sevenseg_pkg is
+	function hex_to_7seg(wxyz : nibble_t) return seg7_t is
+		 variable w, x, y, z : STD_LOGIC;
+		 variable a, b, c, d, e, f, g : STD_LOGIC;
+		 variable seg : seg7_t;
+	begin
+		 w := wxyz(3);
+		 x := wxyz(2);
+		 y := wxyz(1);
+		 z := wxyz(0);
+
+		 a := (not w and not x and not y and z) or
+				(not w and x and not y and not z) or
+				(w and not x and y and z) or
+				(w and x and not y and not z) or
+				(w and x and not y and z);
+
+		 b := (not w and x and not y and z) or
+				(not w and x and y and not z) or
+				(w and not x and y and z) or
+				(w and x and not y and not z) or
+				(w and x and y and not z) or
+				(w and x and y and z);
+
+		 c := (not w and not x and y and not z) or
+				(w and x and not z) or
+				(w and x and y);
+
+		 d := (not x and not y and z) or
+				(not w and x and not y and not z) or
+				(x and y and z) or
+				(w and not x and y and not z);
+
+		 e := (not w and z) or
+				(not w and x and not y) or
+				(not x and not y and z);
+
+		 f := (not w and not x and z) or
+				(not w and not x and y) or
+				(not w and y and z) or
+				(w and x and not y);
+
+		 g := (not w and not x and not y) or
+				(not w and x and y and z);
+
+		 seg(0) := a;
+		 seg(1) := b;
+		 seg(2) := c;
+		 seg(3) := d;
+		 seg(4) := e;
+		 seg(5) := f;
+		 seg(6) := g;
+		 return seg;
+	end function;
+end sevenseg_pkg;
