43d3571da0579a1617e2c91aeeebd0097afacf44
joshbochuan <joshbochuan@gmail.com>
2026-04-13 18:08:33 +0800

    finished partial lab 4 part 2

---
+++ Lab04/target2/alu_pkg.vhd
@@ -0,0 +1,99 @@
+Library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package alu_pkg is
+	component alu1 is
+		port (
+			a: in STD_LOGIC;
+			b: in STD_LOGIC;
+			cin: in std_logic;
+			less: in std_logic;
+			ctrl: in STD_LOGIC_VECTOR(3 downto 0);
+			res: out STD_LOGIC;
+			cout: out std_logic;
+			set: out std_logic
+		);
+	end component;
+	
+	component alu7 is
+	   port(
+		    a: in STD_LOGIC_VECTOR(6 downto 0);
+			 b: in STD_LOGIC_VECTOR(6 downto 0);
+			 cin: in std_logic;
+			 ctrl: in STD_LOGIC_VECTOR(3 downto 0);
+			 res: out STD_LOGIC_VECTOR(6 downto 0);
+			 cout: out std_logic
+		);
+	end component;
+end alu_pkg;
+
+Library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use work.alu_pkg.all;
+
+entity alu1 is
+	port (
+		a: in STD_LOGIC;
+		b: in STD_LOGIC;
+		cin: in std_logic;
+		less: in std_logic;
+		ctrl: in STD_LOGIC_VECTOR(3 downto 0);
+		res: out STD_LOGIC;
+		cout: out std_logic;
+		set: out std_logic
+	);
+end alu1;
+
+architecture structural of alu1 is
+	signal Ainvert: std_logic;
+	signal Bnegate: std_logic;
+	signal AandB: std_logic;
+	signal AorB: std_logic;
+	signal AplusB: std_logic;
+begin
+	Ainvert <= (a xor ctrl(3));
+	Bnegate <= (b xor ctrl(2));
+	AandB <= (Ainvert and Bnegate);
+	AorB <= (Ainvert or Bnegate);
+	AplusB <= (Ainvert xor Bnegate xor cin);
+	cout <= (Ainvert and Bnegate) or (Ainvert and cin) or (Bnegate and cin);
+	res <= (((not ctrl(0)) and (not ctrl(1)) and AandB)
+				or ((ctrl(0) and (not ctrl(1)) and AorB))
+				or ((not ctrl(0)) and ctrl(1) and AplusB)
+				or (ctrl(0) and ctrl(1) and less));
+	set <= AplusB;
+end structural;
+
+Library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use work.alu_pkg.all;
+
+entity alu7 is
+	port(
+		 a: in STD_LOGIC_VECTOR(6 downto 0);
+		 b: in STD_LOGIC_VECTOR(6 downto 0);
+		 cin: in std_logic;
+		 ctrl: in STD_LOGIC_VECTOR(3 downto 0);
+		 res: out STD_LOGIC_VECTOR(6 downto 0);
+		 cout: out std_logic
+	);
+end alu7;
+
+architecture structural of alu7 is
+	signal c : STD_LOGIC_VECTOR(7 downto 0);
+begin
+	c(0) <= cin;
+	GEN_ALU : for i in 0 to 6 generate
+		ALU_INST: alu1 port map(
+			a => a(i),
+			b => b(i),
+			cin => c(i),
+			less => '0',
+			ctrl => ctrl,
+			res => res(i),
+			cout => c(i+1),
+			set => open
+		);
+	end generate GEN_ALU;
+	cout <= c(7);
+end structural;
---
+++ Lab04/target2/target2.vhd
@@ -0,0 +1,33 @@
+Library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use work.all;
+use work.sevenseg_pkg.all;
+
+entity target2 is
+	Port (
+		SW: in std_logic_vector(17 downto 0);
+		HEX0: out std_logic_vector(6 downto 0);
+		HEX1: out std_logic_vector(6 downto 0)
+	);
+end target2;
+
+architecture structure of target2 is
+	signal a: std_logic_vector(6 downto 0);
+	signal b: std_logic_vector(6 downto 0);
+	signal ctrl: std_logic_vector(3 downto 0);
+	signal res: std_logic_vector(6 downto 0);
+begin
+	b <= SW(6 downto 0);
+	a <= SW(13 downto 7);
+	ctrl <= SW(17 downto 14);
+	ALU: alu7 port map (
+		a => a,
+		b => b,
+		cin => '0',
+		ctrl => ctrl,
+		res => res,
+		cout => open
+	);
+	HEX0 <= hex_to_7seg(res(3 downto 0));
+	HEX1 <= hex_to_7seg("0"&res(6 downto 4));
+end structure;
